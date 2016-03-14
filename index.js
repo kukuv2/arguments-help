e49d08b223999dd9cc3b9e92d5b0a247d814bfb7
kukuv2 <xiaorenhui1030@163.com>
2016-03-14 16:47:24 +0800

    添加第一个mocha测试

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
 
     // Baseline setup
     // --------------
@@ -9,36 +9,36 @@
     // Save the previous value of the `Ah` variable.
     var previousUnderscore = root.Ah;
 
-    function extend(obj,source){
-        for(var key in source){
-            obj[key]= source[key]
+    function extend(obj, source) {
+        for (var key in source) {
+            obj[key] = source[key]
         }
         return obj;
     }
 
-    function isArray(arr){
+    function isArray(arr) {
         return toString.call(arr) === '[object Array]'
     }
 
 
     //TODO 生成错误信息
-    function typeEqual(type,arg,executeReportFunction){
-        if(arg === undefined || arg === null){
-            if(arg === type){
+    function typeEqual(type, arg, executeReportFunction) {
+        if (arg === undefined || arg === null) {
+            if (arg === type) {
                 return true;
-            }else{
-                 executeReportFunction('type error');
-            }
-        }else if(toString.call(arg) !== '[object Object]'){
-            if(toString.call(type) === toString.call(arg)){
-                return true;
-            }else {
+            } else {
                 executeReportFunction('type error');
             }
-        }else{
-            if(arg instanceof type){
+        } else if (toString.call(arg) !== '[object Object]') {
+            if (toString.call(type) === toString.call(arg)) {
                 return true;
-            }else {
+            } else {
+                executeReportFunction('type error');
+            }
+        } else {
+            if (arg instanceof type) {
+                return true;
+            } else {
                 executeReportFunction('type error');
             }
         }
@@ -47,62 +47,63 @@
 
     function checkUnitType(argType, arg, executeReportFunction) {
 
-        if(isArray(argType)){
+        if (isArray(argType)) {
             argType.some(function (item) {
-                return typeEqual(item,arg,executeReportFunction);
+                return typeEqual(item, arg, executeReportFunction);
             })
-        }else{
-            return typeEqual(argType,arg,executeReportFunction)
+        } else {
+            return typeEqual(argType, arg, executeReportFunction)
         }
     }
 
 
-    function checkType(argsType,args,executeReportFunction){
-        if(!isArray(argsType)){
-            argsType=[argsType];
+    function checkType(argsType, args, executeReportFunction) {
+        if (!isArray(argsType)) {
+            argsType = [argsType];
         }
-        for(var i = 0;i<args.length;i++){
-            if(!checkUnitType(argsType[i],args[i],executeReportFunction)){
+        for (var i = 0; i < args.length; i++) {
+            if (!checkUnitType(argsType[i], args[i], executeReportFunction)) {
                 return false;
+            } else {
+                return true;
             }
+
         }
     }
 
 
-
-    var Ah = function(args,overLoadings,reportFunction) {
-        return new AhInstance.apply(null,arguments)(overLoadings);
+    var Ah = function (args, overLoadings, reportFunction) {
+        return new AhInstance(args, overLoadings, reportFunction)(overLoadings);
 
     };
 
     Ah.AlertType = console.error;
 
-    function AhInstance(args,overLoading,reportFunction){
-        var thiz = this;
-        thiz.args = args;
-        thiz.overLoading = overLoading;
+    function AhInstance(args, overLoading, reportFunction) {
+        var thiz            = this;
+        thiz.args           = args;
+        thiz.overLoading    = overLoading;
         thiz.reportFunction = reportFunction;
-        return function execute (overLoading,reportFunction) {
-            if(reportFunction){
+        return function execute(overLoading, reportFunction) {
+            if (reportFunction) {
                 thiz.reportFunction = reportFunction
             }
-            if(overLoading){
-                overLoading = extend(thiz.overLoading,overLoading);
+            if (overLoading) {
+                overLoading = extend(thiz.overLoading, overLoading);
             }
 
-            var argsLength = thiz.args.length;
+            var argsLength  = thiz.args.length;
             var executeItem = overLoading[argsLength];
-            if(executeItem){
-                if(!isArray(executeItem)){
-                    executeItem.apply(null,thiz.args);
-                }else{
-                    var executeReportFunction  = executeItem[3] ? executeItem[3] : thiz.reportFunction ? thiz.reportFunction : Ah.AlertType;
-                    if(checkType(executeItem[0],thiz.args,executeReportFunction)){
-                        executeItem.apply(null,thiz.args)
+            if (executeItem) {
+                if (!isArray(executeItem)) {
+                    executeItem.apply(null, thiz.args);
+                } else {
+                    var executeReportFunction = executeItem[3] ? executeItem[3] : thiz.reportFunction ? thiz.reportFunction : Ah.AlertType;
+                    if (checkType(executeItem[0], thiz.args, executeReportFunction)) {
+                        executeItem[1].apply(null, thiz.args)
                     }
                 }
             }
-
 
 
             return execute
@@ -110,12 +111,11 @@
         }
     }
 
-    AhInstance.
 
     Ah.global = function (setting) {
-      for(var i in setting){
-        Ah[i]=setting[i];
-      }
+        for (var i in setting) {
+            Ah[i] = setting[i];
+        }
     };
 
     // Export the arguments-help object for **Node.js**, with
@@ -132,7 +132,7 @@
 
 
     if (typeof define === 'function' && define.amd) {
-        define('arguments-help', [], function() {
+        define('arguments-help', [], function () {
             return Ah;
         });
     }
---
+++ test/test.js
@@ -0,0 +1,18 @@
+var assert = require('assert');
+var Ah     = require('../index');
+describe('arguments-help', function () {
+    it('检查参数类型和处理可选参数', function () {
+        function wrappedFunction() {
+            var intArg = 0, strArg = '', funArg = Function, undefinedType, rangeType = [Number, String];
+            var ah     = Ah(arguments, {
+                1: [[intArg], function (intArg) {
+                    assert(2, arguments.length);
+                    assert(toString.call(arguments[0]) === '[object Number]')
+
+                }]
+            });
+        }
+        wrappedFunction(1)
+
+    });
+});
