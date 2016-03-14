62cbfe30f6b876a34d5fcd88c3b9f555163b40e5
kukuv2 <xiaorenhui1030@163.com>
2016-03-14 14:11:50 +0800

    初步结构

--- README.md
+++ README.md
@@ -1,2 +1,55 @@
 # arguments-help
 javascript function arguments help module
+
+```javascript
+// arguments-help can check arguments's type and handle option arguments
+var Ah = require('arguments-help');
+function wrappedFunction(){
+  var intArg = 0,
+      strArg = '',
+      funArg = Function,
+      definedTypeArg = definedType,
+      undefinedType,
+      rangeType = [Number,String];
+  var Ah = Ah(arguments,{
+        1:[[intArg],function(intArg){
+
+        }],
+        2:[[strArg,rangeType],function(strArg,rangeType){
+
+        }]
+      });
+
+//you can make report type errors in you own way ,just pass third argument.default is console.error().for example:
+  // var Ah = Ah(arguments,{
+  //     1:[[intArg],function(intArg){
+  //
+  //     }],
+  //     2:[[strArg,rangeType],function(strArg,rangeType){
+  //
+  //     }]
+  //   },alert);
+// or set in global
+  // Ah.global({
+  //   AlertType:alert
+  // })
+
+      Ah({
+        3:[[definedType,funArg],function(definedType,funArg,undefinedType){
+
+        },alert]
+      });
+
+//or you can don't check arguments type ,just handle option arguments
+    var Ah = Ah(arguments,{
+      1:function(arg1){
+
+      },
+      2:[function(arg1,arg2){
+
+      },alert]
+
+    })
+
+}
+```
---
+++ index.js
@@ -0,0 +1,118 @@
+(function() {
+
+    // Baseline setup
+    // --------------
+
+    // Establish the root object, `window` in the browser, or `exports` on the server.
+    var root = this;
+
+    // Save the previous value of the `Ah` variable.
+    var previousUnderscore = root.Ah;
+
+    function extend(obj,source){
+        for(var key in source){
+            obj[key]= source[key]
+        }
+        return obj;
+    }
+
+    function isArray(arr){
+        return toString.call(arr) === '[object Array]'
+    }
+
+    function typeEqual(type,arg){
+
+    }
+
+    function checkUnitType(argType, arg, executeReportFunction) {
+
+        if(isArray(argType)){
+            argType.some(function (item) {
+                return typeEqual(item,arg,executeReportFunction);
+            })
+        }else{
+            return typeEqual(argType,arg,executeReportFunction)
+        }
+    }
+
+
+    function checkType(argsType,args,executeReportFunction){
+        if(!isArray(argsType)){
+            argsType=[argsType];
+        }
+        for(var i = 0;i<args.length;i++){
+            if(!checkUnitType(argsType[i],args[i],executeReportFunction)){
+                return false;
+            }
+        }
+    }
+
+
+
+    var Ah = function(args,overLoadings,reportFunction) {
+        return new AhInstance.apply(null,arguments)(overLoadings);
+
+    };
+
+    Ah.AlertType = console.error;
+
+    function AhInstance(args,overLoading,reportFunction){
+        var thiz = this;
+        thiz.args = args;
+        thiz.overLoading = overLoading;
+        thiz.reportFunction = reportFunction;
+        return function execute (overLoading,reportFunction) {
+            if(reportFunction){
+                thiz.reportFunction = reportFunction
+            }
+            if(overLoading){
+                overLoading = extend(thiz.overLoading,overLoading);
+            }
+
+            var argsLength = thiz.args.length;
+            var executeItem = overLoading[argsLength];
+            if(executeItem){
+                if(!isArray(executeItem)){
+                    executeItem.apply(null,thiz.args);
+                }else{
+                    var executeReportFunction  = executeItem[3] ? executeItem[3] : thiz.reportFunction ? thiz.reportFunction : Ah.AlertType;
+                    if(checkType(executeItem[0],thiz.args,executeReportFunction)){
+                        executeItem.apply(null,thiz.args)
+                    }
+                }
+            }
+
+
+
+            return execute
+
+        }
+    }
+
+    AhInstance.
+
+    Ah.global = function (setting) {
+      for(var i in setting){
+        Ah[i]=setting[i];
+      }
+    };
+
+    // Export the arguments-help object for **Node.js**, with
+    // backwards-compatibility for the old `require()` API. If we're in
+    // the browser, add `Ah` as a global object.
+    if (typeof exports !== 'undefined') {
+        if (typeof module !== 'undefined' && module.exports) {
+            exports = module.exports = Ah;
+        }
+        exports.Ah = Ah;
+    } else {
+        root.Ah = Ah;
+    }
+
+
+    if (typeof define === 'function' && define.amd) {
+        define('arguments-help', [], function() {
+            return Ah;
+        });
+    }
+}.call(this));
