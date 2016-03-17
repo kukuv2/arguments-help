93c557b795b75b7e65463eaa855bf6133c1b5031
kukuv2 <xiaorenhui1030@163.com>
2016-03-17 14:29:25 +0800

    修改测试

--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
 var assert = require('assert');
 var Ah     = require('../index');
-describe('arguments-help', function () {
-    it('检查参数类型和处理可选参数', function () {
+var intArg = 0, strArg = '', funArg = Function, undefinedType, rangeType = [Number, String];
+describe('检查参数类型和处理可选参数', function () {
+    it('当只有一个参数,并且参数为Number时', function () {
         function wrappedFunction() {
-            var intArg = 0, strArg = '', funArg = Function, undefinedType, rangeType = [Number, String];
             var ah     = Ah(arguments, {
                 1: [[intArg], function (intArg) {
                     assert(1===arguments.length);
@@ -13,6 +13,20 @@ describe('arguments-help', function () {
             });
         }
         wrappedFunction(1)
-
+    });
+    it('当只有两个及以上参数', function () {
+        function wrappedFunction() {
+            var ah     = Ah(arguments, {
+                1:[[intArg], function () {
+                    assert(false);
+                }],
+                2: [[intArg], function (intArg,str) {
+                    assert(2===arguments.length);
+                    assert(toString.call(arguments[0]) === '[object Number]');
+                    assert(toString.call(arguments[1]) === '[object String]')
+                }]
+            });
+        }
+        wrappedFunction(1,'str')
     });
 });
