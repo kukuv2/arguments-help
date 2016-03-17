e2f1f577de8b3d1bc4a0058be055cfc9b27dc513
kukuv2 <xiaorenhui1030@163.com>
2016-03-17 16:08:32 +0800

    支持 Number,String

--- test/test.js
+++ test/test.js
@@ -2,34 +2,34 @@ var assert = require('assert');
 var Ah     = require('../index');
 var intArg = 0, strArg = '', funArg = Function, undefinedType, rangeType = [Number, String];
 describe('检查参数类型和处理可选参数', function () {
-    //it('当只有一个参数,并且参数为Number时', function () {
-    //    function wrappedFunction() {
-    //        var ah = Ah(arguments, {
-    //            1: [intArg, function (intArg) {
-    //                assert(1 === arguments.length);
-    //                assert(toString.call(arguments[0]) === '[object Number]')
-    //
-    //            }]
-    //        });
-    //    }
-    //
-    //    wrappedFunction(1)
-    //});
-    //it('当只有两个及以上参数', function () {
-    //    function wrappedFunction() {
-    //        var ah = Ah(arguments, {
-    //            1: [[intArg], function () {
-    //                assert(false);
-    //            }], 2: [[intArg], function (intArg, str) {
-    //                assert(2 === arguments.length);
-    //                assert(toString.call(arguments[0]) === '[object Number]');
-    //                assert(toString.call(arguments[1]) === '[object String]')
-    //            }]
-    //        });
-    //    }
-    //
-    //    wrappedFunction(1, 'str')
-    //});
+    it('当只有一个参数,并且参数为Number时', function () {
+        function wrappedFunction() {
+            var ah = Ah(arguments, {
+                1: [intArg, function (intArg) {
+                    assert(1 === arguments.length);
+                    assert(toString.call(arguments[0]) === '[object Number]')
+
+                }]
+            });
+        }
+
+        wrappedFunction(1)
+    });
+    it('当只有两个及以上参数', function () {
+        function wrappedFunction() {
+            var ah = Ah(arguments, {
+                1: [[intArg], function () {
+                    assert(false);
+                }], 2: [[intArg], function (intArg, str) {
+                    assert(2 === arguments.length);
+                    assert(toString.call(arguments[0]) === '[object Number]');
+                    assert(toString.call(arguments[1]) === '[object String]')
+                }]
+            });
+        }
+
+        wrappedFunction(1, 'str')
+    });
     it('测试参数类型范围', function () {
         function wrappedFunction() {
             var ah = Ah(arguments, {
